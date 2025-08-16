fc15020c3a989e9a7686f9630ce42dac4b8c16cd
Ze40 <maxim.schcerbakov@gmail.com>
2025-08-16 12:12:58 +0300

    fix: import dotenv

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
     [
       'module:react-native-dotenv',
       {
-        moduleName: '@env',
+        moduleName: 'env-config',
         path: '.env',
         safe: false,
         allowUndefined: true,
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,3 +1,3 @@
-declare module '@env' {
+declare module 'env-config' {
   export const API_KEY: string;
 }
